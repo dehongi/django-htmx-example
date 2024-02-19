c2169fa233b78ea05a2f765f512e8943e944dc1b
Hamed Mohammadi <hamed@dehongi.com>
2024-02-19 21:26:47 +0330

    mark task completed

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("<int:pk>/", views.TaskDetailView.as_view(), name="task_detail"),
     path("<int:pk>/edit/", views.TaskUpdateView.as_view(), name="task_edit"),
     path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task_delete"),
+    path(
+        "<int:task_id>/mark_task_completed/",
+        views.mark_task_completed,
+        name="mark_task_completed",
+    ),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse
 
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+
+
 from django.views import generic
 
 from .models import Task
@@ -13,6 +17,17 @@ class TaskListView(generic.ListView):
     model = Task
     context_object_name = "tasks"
     paginate_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(completed=False)
+
+
+def mark_task_completed(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.completed = True
+    task.save()
+    # Optionally, you can return a JSON response indicating success
+    return JsonResponse({"message": "Task marked as completed successfully"})
 
 
 class TaskCreateView(generic.CreateView):
